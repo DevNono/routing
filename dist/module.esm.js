8bb0897836dbf7b633040dcbfed045a20eb05b11
Noé Landré <noe.landre@laposte.net>
2021-07-08 12:29:25 +0200

    merge ajax-form and routing

--- dist/module.esm.js
+++ dist/module.esm.js
@@ -1,8 +1,36 @@
 // src/index.js
+var idHTMLToReplace = "main";
 function src_default(Alpine) {
   Alpine.directive("routing", (el, { expression }, { evaluate }) => {
     el.addEventListener("click", () => {
-      change_page(get_base_url() + evaluate(expression), "main");
+      change_page(get_base_url() + evaluate(expression), idHTMLToReplace);
+    });
+  });
+  Alpine.directive("ajax", (el) => {
+    if (el.nodeName != "FORM") {
+      return;
+    }
+    el.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      var formData = new FormData(el);
+      var url = el.getAttribute("action").includes("http") ? el.getAttribute("action") : api_base_domain + el.getAttribute("action");
+      var method = el.getAttribute("method");
+      var resp = await makeRequestCreator(url, method, formData);
+      if (resp != null) {
+        el._x_dataStack[0].errors = {};
+        if (resp.isAxiosError) {
+          console.log(resp);
+        } else {
+          if (resp.data.errors != void 0) {
+            for (const [key, value] of Object.entries(resp.data.errors)) {
+              el._x_dataStack[0].errors[key] = value;
+            }
+          }
+          el._x_dataStack[0].status = resp.data.status;
+          el._x_dataStack[0].message = resp.data.message;
+          el._x_dataStack[0].data = resp.data.data;
+        }
+      }
     });
   });
 }
@@ -10,19 +38,49 @@ function get_base_url() {
   var getUrl = window.location;
   return getUrl.protocol + "//" + getUrl.host;
 }
-async function change_page(url, idHTMLToReplace) {
-  var resp = await axios.get(url, {
-    validateStatus: function(status) {
-      return status < 500;
-    }
-  }).then((response) => {
-    return response.data;
-  });
+window.change_page = async function(url, idHTMLToReplace2) {
+  var resp = await makeRequestCreator(url, "get");
+  resp = resp.data;
   history.pushState(null, "", url);
   var el = document.createElement("html");
   el.innerHTML = resp;
-  var main = el.querySelector("div#" + idHTMLToReplace).innerHTML;
-  document.querySelector("div#" + idHTMLToReplace).innerHTML = main;
+  var main = el.querySelector("div#" + idHTMLToReplace2).innerHTML;
+  document.querySelector("div#" + idHTMLToReplace2).innerHTML = main;
+};
+function makeRequestCreator() {
+  var call;
+  return async function(url, method, formData = "") {
+    if (call) {
+      call.cancel();
+    }
+    call = axios.CancelToken.source();
+    if (method == "post" || method == "put") {
+      await axios.get(base_domain + "/sanctum/csrf-cookie").catch(function(err) {
+        console.log(err);
+      });
+    }
+    return await axios({
+      url,
+      data: formData,
+      method,
+      cancelToken: call.token,
+      validateStatus: function(status) {
+        return status < 500;
+      }
+    }).then((response) => {
+      if (response.status == 302) {
+        change_page(response.headers["Location"], idHTMLToReplace);
+        return null;
+      }
+      return response;
+    }).catch(function(thrown) {
+      if (axios.isCancel(thrown)) {
+        console.log("First request canceled", thrown.message);
+      } else {
+        console.log(thrown);
+      }
+    });
+  };
 }
 
 // builds/module.js
--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-export default function (Alpine) {
-    //(replaced by axios-progress-bar)
-    // window.progressBar = document.querySelector("#progress-fill");
-    // window.progressOn = true;
-    // if(window.progressBar == null){
-    //     window.progressOn = false;
-    //     console.log(window.progressBar);
-    // }
+var idHTMLToReplace = 'main';
 
+export default function (Alpine) {
     Alpine.directive('routing', (el, { expression }, { evaluate }) => {
         el.addEventListener('click', () => {
-            change_page(get_base_url() + evaluate(expression), 'main')
+            change_page(get_base_url() + evaluate(expression), idHTMLToReplace)
+        })
+    })
+
+    Alpine.directive('ajax', (el) => {
+        if(el.nodeName != "FORM"){
+            return;
+        }
+    
+        el.addEventListener('submit', async (event) => {
+            // cancel normal submit
+            event.preventDefault();
+    
+            // pack form data
+            var formData = new FormData(el);
+    
+            // get data from form
+            var url = el.getAttribute("action").includes('http') ? el.getAttribute("action") : api_base_domain + el.getAttribute("action");
+            var method = el.getAttribute("method");
+    
+            var resp = await makeRequestCreator(url, method, formData);
+            if(resp != null){
+                el._x_dataStack[0].errors = {};
+                if(resp.isAxiosError){
+                    console.log(resp);
+                }else{
+                    if(resp.data.errors != undefined){
+                        for (const [key, value] of Object.entries(resp.data.errors)) {
+                            el._x_dataStack[0].errors[key] = value;
+                        }
+                    }
+                    el._x_dataStack[0].status = resp.data.status;
+                    el._x_dataStack[0].message = resp.data.message;
+                    el._x_dataStack[0].data = resp.data.data;
+                }
+                
+            }
         })
     })
 }
@@ -20,76 +50,9 @@ function get_base_url(){
     return getUrl .protocol + "//" + getUrl.host;
 }
 
-//Update progress bar (replaced by axios-progress-bar)
-// window.progress = function(loaded, total) {
-//     if(window.progressOn){
-//         window.progressBar.style.width = Math.round(loaded/total*100) +'%';
-//     }
-// }
-
-async function change_page(url, idHTMLToReplace){
-    // old fetchAPI
-    /* var resp = await fetch(url).then(response => {
-        console.log(response);
-        if (!response.ok) {
-            throw Error(response.status+' '+response.statusText)
-        }
-    
-        if (!response.body) {
-            throw Error('ReadableStream not yet supported in this browser.')
-        }
-    
-        // to access headers, server must send CORS header "Access-Control-Expose-Headers: content-encoding, content-length x-file-size"
-        // server must send custom x-file-size header if gzip or other content-encoding is used
-        const contentEncoding = response.headers.get('content-encoding');
-        const contentLength = response.headers.get(contentEncoding ? 'x-file-size' : 'content-length');
-        if (contentLength === null) {
-            throw Error('Response size header unavailable');
-        }
-    
-        const total = parseInt(contentLength, 10);
-        let loaded = 0;
-    
-        return new Response(
-        new ReadableStream({
-            start(controller) {
-            const reader = response.body.getReader();
-    
-            read();
-            function read() {
-                reader.read().then(({done, value}) => {
-                    if (done) {
-                        controller.close();
-                        return; 
-                    }
-
-                    loaded += value.byteLength;
-                    progress(loaded, total);
-
-                    controller.enqueue(value);
-                    read();
-                }).catch(error => {
-                    console.error(error);
-                    controller.error(error)                  
-                })
-            }
-            }
-        })
-        );
-    })
-    .then((response) => { return response.text(); })
-    .catch(error => {
-        console.error(error);
-    }) */
-
-    var resp = await axios.get(url,
-    {
-        validateStatus: function (status) {
-            return status < 500; // Resolve only if the status code is less than 500
-        }
-    }).then((response) => {
-        return response.data;
-    });
+window.change_page = async function (url, idHTMLToReplace){
+    var resp = await makeRequestCreator(url, 'get');
+    resp = resp.data;
 
     history.pushState(null, '', url);
 
@@ -97,4 +60,46 @@ async function change_page(url, idHTMLToReplace){
     el.innerHTML = resp;
     var main = el.querySelector('div#' + idHTMLToReplace).innerHTML;
     document.querySelector('div#' + idHTMLToReplace).innerHTML = main;
+}
+
+function makeRequestCreator() {
+    var call;
+    return async function(url, method, formData = '') {
+        if (call) {
+            call.cancel();
+        }
+        call = axios.CancelToken.source();
+
+        if(method == 'post' || method == 'put'){
+            await axios.get(base_domain + '/sanctum/csrf-cookie').catch(function (err) {
+                //handle error
+                console.log(err);
+            });
+        }
+
+        return await axios({ 
+            url: url,
+            data: formData,
+            method: method,
+            cancelToken: call.token,
+            validateStatus: function (status) {
+                return status < 500; // Resolve only if the status code is less than 500
+            },
+            
+        }).then((response) => {
+            
+            if(response.status == 302){
+                change_page(response.headers['Location'], idHTMLToReplace);
+                return null;
+            }
+       
+            return response;
+        }).catch(function(thrown) {
+            if (axios.isCancel(thrown)) {
+                console.log('First request canceled', thrown.message);
+            } else {
+                console.log(thrown);
+            }
+        });
+    }
 }
